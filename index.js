3ec9af2b6f05780c59f973821a96be000bf989b5
Zahra Khan <zarssskhan@gmail.com>
2017-09-17 03:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,9 +75,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
     return false;
-  } else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`);
   }
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`);
 
   cart = [];
 };
